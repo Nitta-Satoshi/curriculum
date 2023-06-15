e390701f271595cf047d831d372ede552c232e6a
Nitta-Satoshi <s-nitta@si.cmind-co.jp>
2023-06-15 23:09:31 +0900

    5章課題テスト
    ５章チェックテストの提出

---
+++ JavaStudy/5-1/src/Main.java
@@ -0,0 +1,67 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		{
+//      問① 下記例の配列をString型しか格納できないように修正してください。
+//		現状は全てのクラスの継承元であるObject型を指定しているので、Integerもintも格納可能になっています。
+//	       例List<Object> array = new ArrayList<>();
+//		また、arrayには[hoge, pos, foo]の文字列３要素を入れてください。
+//	    <以下記述>
+			List<String> array = new ArrayList<String>();
+			array.add("hoge");
+			array.add("pos");
+			array.add("foo");
+
+//      問② arrayの二つ目の要素を"bar"にしましょう。
+			array.set(1, "bar");
+//      問③ fooが格納されているインデックスを出力してください。
+			int i = 2;
+	            System.out.println(array.get(i) + "のインデックス：" + i);
+			
+		}
+
+		{
+//      問④ キーがString型、バリューがObject型の要素を三つ格納しましょう。
+		Map<String, Object> map = new HashMap<>();
+		map.put("address", "/////");
+		map.put("name", "Satoshi");
+		map.put("age", "23");
+
+//      問⑤上記で格納したキーを繰り返し文で出力しましょう。
+        for (Entry<String, Object> list : map.entrySet()) {
+            System.out.println("key : " + list.getKey());
+        }
+
+//      問⑥上記で格納したバリューを繰り返し文で出力しましょう。
+        for (Entry<String, Object> list : map.entrySet()) {
+            System.out.println("value : " + list.getValue());
+        }
+		{
+//	      問⑦ Calender型calを使い、int型配列arrayDateに今の年・月・日を入れてください。
+	           Calendar cal = Calendar.getInstance();
+//		    <以下記述>
+	           Integer [] arrayDate = {cal.get(Calendar.YEAR), cal.get(Calendar.MONTH) + 1, cal.get(Calendar.DATE)};
+//	      問⑧ 配列arrayDateをList型に置換しましょう。
+//	      ヒント：asListメソッドはプリミティブ型のデータ型のみ扱えます。
+//		    <以下記述>
+	           List<Integer> listDate = new ArrayList<Integer>(Arrays.asList(arrayDate));
+		//	      問⑨ 上記で格納した要素を繰り返しで出力しましょう。
+//		    <以下記述>
+        for (Integer date : listDate) {
+            System.out.println(date);
+        }
+		}
+		}
+	}
+
+}
